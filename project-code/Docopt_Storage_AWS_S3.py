d7c7ed34e2704c4ef58f189f6e47df9e5209ef6a
trawat87 <44417270+trawat87@users.noreply.github.com>
2019-03-15 11:00:33 -0400

    Update Docopt_Storage_AWS_S3.py
    Update file as per discussion with Gregor on 3/13 to ensure that AWS, AZURE and BOX have same abstraction

--- project-code/Docopt_Storage_AWS_S3.py
+++ project-code/Docopt_Storage_AWS_S3.py
@@ -1,79 +1,63 @@
-document what a container is
-
-container could be doen with a default?
-
 """
 Usage:
-    cms storage [--cloud=<SERVICE>] list containers
-    cms storage [--cloud=<SERVICE>] create container CONTAINER
-    cms storage [--cloud=<SERVICE>] delete container CONTAINER
-    cms storage [--cloud=<SERVICE>] put file CONTAINER FILESOURCE FILETARGET
-    cms storage [--cloud=<SERVICE>] delete file CONTAINER FILENAME
-    cms storage [--cloud=<SERVICE>] get file CONTAINER FILENAME
-    cms storage [--cloud=<SERVICE>] list container files CONTAINER
-    cms storage [--cloud=<SERVICE>] list file info CONTAINER FILENAME ...
+    storage [--storage=<SERVICE>] create dir DIRNAME
+    storage [--storage=<SERVICE>] delete dir DIRNAME
+    storage [--storage=<SERVICE>] list dir files [DIRNAME]
+    storage [--storage=<SERVICE>] put file SOURCEFILENAME SOURCEDIR DESTFILENAME DESTDIR
+    storage [--storage=<SERVICE>] get file SOURCEFILENAME SOURCEDIR DESTFILENAME DESTDIR
+    storage [--storage=<SERVICE>] delete file FILENAME DIRNAME
+    storage [--storage=<SERVICE>] search file FILENAME [DIRNAME]
+    storage [--storage=<SERVICE>] list file info FILENAME DIRNAME
 
 Manage file storage on AWS S3 buckets and perform operations like put, get, delete on the files.
 
 Arguments:
-    CONTAINER   Mame of the logical container to host a file. E.g this will be the bucket name on AWS S3
-    FILESOURCE  Complete file path (including the directory and filename) of the local file to be uploaded on cloud
-    FILETARGET  Complete file path (including the directory and filename) where the file is to be uploaded on cloud
-    FILENAME    Complete file path (including the directory and filename) on the cloud
+    DIRNAME Name of the directory where file is to be created or searched or deleted.
+    FILENAME Name of the file is to be created or searched or deleted.
+    SOURCEFILENAME Name of the source file for put or get actions
+    SOURCEDIR Name of the source file directory for put or get actions
+    DESTFILENAME Name of the destination file for put or get actions
+    DESTDIR Name of the destination file directory for put or get actions
 
 Options:
   -h --help
-  --cloud=<SERVICE>  Cloud service name like aws or azure or box or google [default: aws]
+  --storage=<SERVICE>  Cloud storage service name like aws or azure or box or google
 
 Description:
-    Commands to manage file storage on cloud/
+    Commands to manage file storage on cloud
 
-    storage listcontainers
-        lists all containers present on the cloud storage
+    storage create dir
+        Creates directory with the given name.
 
-    storage createcontainer CONTAINER
-        creates a container with the given name in the cloud storage
+    storage delete dir
+        Deletes directory with the given name.
 
-    storage deletecontainer CONTAINER
-        deletes a container with the given name in the cloud storage
+    storage list dir files
+        Lists all files present in the input directory.
+        If no dir is specified, it will list all files across directories.
 
-    storage putfile CONTAINER FILESOURCE FILETARGET
-        uploads a file from the source file path to the target file on the cloud storage in the specified container
+    storage put file
+        Uploads file to cloud storage from the local store.
 
-    storage deletefile CONTAINER FILENAME
-        deletes a file from the specified container on the cloud storage
+    storage get file
+        Downloads file from cloud storage to the local store.
 
-    storage getfile CONTAINER FILENAME
-        downloads a file from the specified container on the cloud storage
+    storage delete file
+        Deletes the input file from specified cloud storage directory.
 
-    storage listcontainerfiles CONTAINER
-        lists all file in the specified container on the cloud storage
+    storage search file
+        Searches and lists the input file from specified cloud storage directory.
+        If no dir is specified, it will list all files across directories which match the filename.
 
-    storage listfileinfo CONTAINER [FILENAME] ...
-        returns properties of the specified files in the container on the cloud storage
+    storage list file info
+        Lists the file attributes for the input file.
 
+Example:
+    set storage=aws
+    storage put FILENAME DESTDIR
 
-Examples:
-replace python Docopt_Storage_AWS_S3.py  with cms
-
-   python Docopt_Storage_AWS_S3.py storage listcontainers
-
-    python Docopt_Storage_AWS_S3.py storage listcontainers --cloud=aws
-
-    python Docopt_Storage_AWS_S3.py storage createcontainer my_container
-
-    python Docopt_Storage_AWS_S3.py storage deletecontainer my_container
-
-    python Docopt_Storage_AWS_S3.py storage putfile my_container file1 file2
-
-    python Docopt_Storage_AWS_S3.py storage	deletefile my_container file1
-
-    python Docopt_Storage_AWS_S3.py storage	getfile my_container file1
-
-    python Docopt_Storage_AWS_S3.py storage	listcontainerfiles my_container
-
-    python Docopt_Storage_AWS_S3.py storage listfileinfo my_container file1 file2
-
+    is the same as
+    storage  --storage=aws put FILENAME DESTDIR
 """
 
 from docopt import docopt
