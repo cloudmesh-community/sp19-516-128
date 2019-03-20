9442e4d4826c3dd0329f153c6e3a9ea7d0791648
trawat87 <44417270+trawat87@users.noreply.github.com>
2019-03-20 18:58:37 -0400

    Update manager.py

--- project-code/manager.py
+++ project-code/manager.py
@@ -10,52 +10,52 @@ class Manager(object):
 
     def _provider(self, service):
         provider = None
-        if self.service == "gdrive":
+        if service == "gdrive":
             provider = cloudmesh.storage.provider.gdrive.Provider.Provider()
-        elif self.service == "box":
+        elif service == "box":
             provider = cloudmesh.storage.provider.box.Provider.Provider()
-        elif self.service == "aws":
+        elif service == "aws":
             provider = cloudmesh.storage.provider.aws.Provider.Provider()
         return provider
 
 
     def createDir(self, service, dirName):
-        print("put", service, dirName)
+        print("createDir", service, dirName)
         provider = self._provider(service)
         provider.createDir(dirName)
 
     def listDirFiles(self, service, dirName):
-        print("put", service, dirName)
+        print("listDirFiles", service, dirName)
         provider = self._provider(service)
         provider.listDirFiles(dirName)
 
     def deleteDir(self, service, dirName):
-        print("put", service, dirName)
+        print("deleteDir", service, dirName)
         provider = self._provider(service)
         provider.deleteDir(dirName)
 
     def putFile(self, service, sourceFilename, sourceDir, destFilename, destDir):
-        print("put", service, sourceFilename, sourceDir, destFilename, destDir)
+        print("putFile", service, sourceFilename, sourceDir, destFilename, destDir)
         provider = self._provider(service)
         provider.putFile(sourceFilename, sourceDir, destFilename, destDir)
 
     def getFile(self, service, sourceFilename, sourceDir, destFilename, destDir):
-        print("put", service, sourceFilename, sourceDir, destFilename, destDir)
+        print("getFile", service, sourceFilename, sourceDir, destFilename, destDir)
         provider = self._provider(service)
         provider.getFile(sourceFilename, sourceDir, destFilename, destDir)
 
     def deleteFile(self, service, fileName, dirName):
-        print("put", service, fileName, dirName)
+        print("deleteFile", service, fileName, dirName)
         provider = self._provider(service)
         provider.deleteFile(fileName, dirName)
 
     def searchFile(self, service, fileName, dirName):
-        print("put", service, fileName, dirName)
+        print("searchFile", service, fileName, dirName)
         provider = self._provider(service)
         provider.searchFile(fileName, dirName)
 
     def listFileInfo(self, service, fileName, dirName):
-        print("put", service, fileName, dirName)
+        print("listFileInfo", service, fileName, dirName)
         provider = self._provider(service)
         provider.listFileInfo(fileName, dirName)
 
